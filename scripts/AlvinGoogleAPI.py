88bc99d0c6251630e43dff73af046c9422a60df3
AlvinHuang13 <Alvinhuang13@gmail.com>
2022-03-06 03:57:11 -0800

    Update AlvinGoogleAPI.py

--- scripts/AlvinGoogleAPI.py
+++ scripts/AlvinGoogleAPI.py
@@ -27,7 +27,7 @@ def get_image(Address,satellite=False):
     r.raise_for_status()
     temp = r.content
     folder = 'satellite_view' if satellite else 'roadmap_view'
-    with open(f'{folder}/{Address.strip()}.png','wb') as file:
+    with open(f'../{folder}/{Address.strip()}.png','wb') as file:
         file.write(temp)
 
 
